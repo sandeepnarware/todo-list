4dc136f8e6441d0910bd80f1ef5ca65faed629e1
Sandeep Kumar Narware <snarware@marginpoint.com>
2026-06-19 23:18:25 +0530

    Add PWA manifest, service worker, icon

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'todo-pomo-v1';
+const URLS = ['/', '/todo-list/', '/index.html', '/style.css', '/app.js', '/icon.svg', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
